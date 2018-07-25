37276a38916ba50eb0cf1f30fb1c37f5fe35fbe1
osama-khalid <khalyd.osama@gmail.com>
2018-07-25 10:55:09 -0500

    Add files via upload

--- ResultView.py
+++ ResultView.py
@@ -1,6 +1,8 @@
 import requests
 import json
 import operator
+import time
+from bs4 import BeautifulSoup as BS
 File=open('NA_seats_2018.csv').read().split('\n')
 local={}
 order=[]
@@ -9,34 +11,39 @@ for i in range(1,len(File)):
         row=File[i].split(',')
         local[row[0]]=[row[2],row[3]]
         order.append(row[0])
-
-result=requests.get('https://election-res.herokuapp.com/api/results')
-result=json.loads(result.content)
-seat={}
-for i in range(0,len(result)):
-    row=result[i]
-    seat[row['seat']]=i
-    
-SortSeat=sorted(seat.keys(),key=operator.itemgetter(1))    
-SortSeat.sort()
-print("Province",'-',"District",'-',"Constituency",' - ',"Candidate",' - ',"Party",' - ',"Total Votes")
-print(" ")
-FinalRank={}
-listing=[]
-for row in order:
-    if row in SortSeat:
-        L=len(result[seat[row]]['results'])
-        if result[seat[row]]['results'][0]['party'] not in FinalRank:
-            FinalRank[result[seat[row]]['results'][0]['party']]=0
-        FinalRank[result[seat[row]]['results'][0]['party']]+=1    
-        for i in range(L):
-            print(local[row][1],'-',local[row][0],'-',result[seat[row]]['seat'],' - ',result[seat[row]]['results'][i]['candidate'],' - ',result[seat[row]]['results'][i]['party'],' - ',result[seat[row]]['results'][i]['votes'])
-            
-        print("Total Votes:",result[seat[row]]['Votes Polled'],"Valid Votes:",result[seat[row]]['Valid Votes'],"Rejected Votes:",result[seat[row]]['Rejected Votes'])
-        print(" ")
-
-
-Ranks=sorted(FinalRank.items(),key=operator.itemgetter(1),reverse=True)            
+slp=0
+Total={}
+file=open('ElectionResults.csv','w')
+for S in order:        
+    slp=slp+1
+    if slp%50==0:
+     	
+        time.sleep(4)
+    r=requests.post("https://www.geo.tv/election/election_constituency_result", data={'seat':S})
+    page=r.content
+    soup = BS(page, 'html.parser')
+    seat=soup.find('h2').text
+    region=local[seat][0]
+    province=local[seat][1]
+    candidates=soup.find('tbody').find_all('tr')
+    flag=1
+    for i in range(0,len(candidates)):
+        endname=str(candidates[i].find('h5')).find("<s")
+        name=candidates[i].find('h5').text[:endname-4]
+        party=candidates[i].find('h5').text[endname-4:]
+        votes=candidates[i].find_all('td')[1].text
+        if i==0:
+            if party not in Total:
+                Total[party]=0
+            Total[party]+=1    
+        if votes!="-":
+            print(seat,region,province,name,party,votes)
+            file.write(seat+","+region+","+province+","+name+","+party+","+votes)
+            flag=0
+    if flag==0:
+        print(" ")        
+file.close()        
+Ranks=sorted(Total.items(),key=operator.itemgetter(1),reverse=True)            
 print('Party - Seats')
 for r in Ranks:
-    print(r[0],'-',r[1])
+    print(r[0],' ',r[1])            
