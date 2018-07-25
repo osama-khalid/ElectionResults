9d84ef8297317a184a43df1ce23a9e2729e7a819
osama-khalid <khalyd.osama@gmail.com>
2018-07-25 12:22:06 -0500

    Add files via upload

--- ResultView.py
+++ ResultView.py
@@ -11,22 +11,29 @@ for i in range(1,len(File)):
         row=File[i].split(',')
         local[row[0]]=[row[2],row[3]]
         order.append(row[0])
-slp=0
+
 Total={}
 file=open('ElectionResults.csv','w')
 for S in order:        
-    slp=slp+1
-    if slp%50==0:
+    
      	
-        time.sleep(4)
-    r=requests.post("https://www.geo.tv/election/election_constituency_result", data={'seat':S})
-    page=r.content
-    soup = BS(page, 'html.parser')
-    seat=soup.find('h2').text
-    region=local[seat][0]
-    province=local[seat][1]
-    candidates=soup.find('tbody').find_all('tr')
-    flag=1
+        
+    while True:
+        try:
+            r=requests.post("https://www.geo.tv/election/election_constituency_result", data={'seat':S})
+            page=r.content
+            soup = BS(page, 'html.parser')
+            seat=soup.find('h2').text
+            region=local[seat][0]
+            province=local[seat][1]
+            candidates=soup.find('tbody').find_all('tr')
+            flag=1
+            break
+        except:
+            print('wait')
+            time.sleep(10)
+            
+    
     for i in range(0,len(candidates)):
         endname=str(candidates[i].find('h5')).find("<s")
         name=candidates[i].find('h5').text[:endname-4]
@@ -38,7 +45,7 @@ for S in order:
             Total[party]+=1    
         if votes!="-":
             print(seat,region,province,name,party,votes)
-            file.write(seat+","+region+","+province+","+name+","+party+","+votes)
+            file.write(seat+","+region+","+province+","+name+","+party+","+votes+'\n')
             flag=0
     if flag==0:
         print(" ")        
